e2e0ccd021ea5b3f8a3a9613274d33d1d0cc3c04
egorkarabanov <egor.karabanov@gmail.com>
2023-08-04 17:22:16 +0200

    created base for project

--- shop/models.py
+++ shop/models.py
@@ -17,5 +17,31 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-    def get_absolute_url(self):
-        return
+    # def get_absolute_url(self):
+    #     return
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200)
+    description = models.TextField(blank=True)
+    category = models.ForeignKey(Category, related_name='products',on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['id', 'slug']),
+            models.Index(fields=['name']),
+            models.Index(fields=['-created']),
+        ]
+
+    def __str__(self):
+        return self.name
+
+    # def get_absolute_url(self):
+    #     return
